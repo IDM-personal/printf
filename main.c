6cf4ef268112f6ad25cd5a0bcc91c3652744d9cc
IDM <idelgadomecinas@gmail.com>
2021-01-12 21:21:19 +0100

    rbstr

--- main.c
+++ main.c
@@ -8,6 +8,6 @@ int		main()
 
 	val = 0;
 	//printf("LAST LENGTH = %i \n",ft_printf("%s","10"));
-	val = ft_printf("hello, %s.", NULL);
+	val = ft_printf("%3.s", NULL);
 	printf("MYVAL %i\n",val);
 }

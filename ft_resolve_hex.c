9889f5487d4331ce3b8fb64b177f68eceae52678
IDM <idelgadomecinas@gmail.com>
2021-01-12 23:44:27 +0100

    hex

--- ft_resolve_hex.c
+++ ft_resolve_hex.c
@@ -6,7 +6,7 @@
 /*   By: idm <idm@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 01:49:32 by idm               #+#    #+#             */
-/*   Updated: 2021/01/12 23:40:38 by idm              ###   ########.fr       */
+/*   Updated: 2021/01/12 23:44:13 by idm              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void ft_resolve_hex(char *cad, t_flag *flg, int *flenght)
     int ilength;
 
     ilength = ft_strlen(cad);
+    if(flg->width == 0 || flg->precision == 0)
+    {
+        *flenght = 0;
+        return ;
+    }
     if(flg->width != -1 || flg->precision != -1)
     {
         if(flg->precision > flg->width)

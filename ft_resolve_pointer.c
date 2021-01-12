f1c309f9e12eef1d5c20f2ea793c2e26fecff1ca
IDM <idelgadomecinas@gmail.com>
2021-01-13 00:23:41 +0100

    ptr

--- ft_resolve_pointer.c
+++ ft_resolve_pointer.c
@@ -6,7 +6,7 @@
 /*   By: idm <idm@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 01:49:32 by idm               #+#    #+#             */
-/*   Updated: 2021/01/13 00:20:36 by idm              ###   ########.fr       */
+/*   Updated: 2021/01/13 00:22:14 by idm              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void ft_resolve_pointer(char *cad, t_flag *flg, int *flenght)
 
     ilength = ft_strlen(cad);
     *flenght += 2;
-    ft_putstr_fd("0x",1);
     if(flg->width != -1 || flg->precision != -1)
     {
         if(flg->precision > flg->width)

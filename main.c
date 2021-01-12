d4f723f7a57a8761f1e7f845799924b4175516b1
IDM <idelgadomecinas@gmail.com>
2021-01-12 08:23:42 +0100

    str

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 
 int		main()
 {
-	ft_printf("%32s", "abc");
+	int val;
+
+	val = 0;
+	val = ft_printf("%7.5s", "yolo");
+	printf("\nRETURN : %i",val);
 	//printf("LAST LENGTH = %i \n",ft_printf("%s","10"));
+	ft_printf("\n%s", NULL);
+	printf("\n%s", NULL);
 }

35fd7911ff14faaaed40dee4007cb3f95c948224
IDM <idelgadomecinas@gmail.com>
2021-01-13 07:53:34 +0100

    arr

--- ft_resolve_pct.c
+++ ft_resolve_pct.c
@@ -6,7 +6,7 @@
 /*   By: idm <idm@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 05:20:26 by idm               #+#    #+#             */
-/*   Updated: 2021/01/13 07:30:30 by idm              ###   ########.fr       */
+/*   Updated: 2021/01/13 07:53:24 by idm              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ void ft_resolve_pct(char pct, t_flag *flg, int *flenght)
     {
         if(flg->leftjust)
         {
-            ft_putchar_fd(pct, 1);
+            ft_putchar(pct, flenght);
             ft_putblanks(flg->width - 1, flenght);
         }
         else if(flg->zero)
         {
             ft_putzeros(flg->width - 1, flenght);
-            ft_putchar_fd(pct, 1);
+            ft_putchar(pct, flenght);
         }
         else
         {
             ft_putblanks(flg->width - 1, flenght);
-            ft_putchar_fd(pct, 1);
+            ft_putchar(pct, flenght);
         }
         return ;
     }
     else
-        ft_putchar_fd(pct, 1);
+        ft_putchar(pct, flenght);
 }

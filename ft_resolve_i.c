e5c09fa46b909c3f45f8b12ab8cdff07b38a4435
root <root@DESKTOP-47KD6QM.localdomain>
2021-01-19 19:35:00 +0100

    i

--- ft_resolve_i.c
+++ ft_resolve_i.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 19:03:55 by root              #+#    #+#             */
-/*   Updated: 2021/01/19 19:33:29 by root             ###   ########.fr       */
+/*   Updated: 2021/01/19 19:34:55 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void ft_resolve_i(int i, t_flag *flg, int *flenght)
         cad = ft_itoa(i * -1);
         neg = 1;
     }
-    printf("NEG %i\n",neg);
     if(flg->width != -1 || flg->precision != -1)
     {
         if(flg->precision > flg->width)

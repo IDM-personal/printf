bdea15da9c6239846370b3422c44aced5fdd888e
root <root@DESKTOP-47KD6QM.localdomain>
2021-01-20 10:13:54 +0100

    i

--- ft_resolve_i.c
+++ ft_resolve_i.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 19:03:55 by root              #+#    #+#             */
-/*   Updated: 2021/01/20 10:08:03 by root             ###   ########.fr       */
+/*   Updated: 2021/01/20 10:13:50 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void ft_resolve_i(int i, t_flag *flg, int *flenght)
             }
             else
             {
-                printf("%i <> %i <> %i <> %i <> %i\n", flg->zero, flg->width, flg->precision, ilength, neg);
+                //printf("%i <> %i <> %i <> %i <> %i\n", flg->zero, flg->width, flg->precision, ilength, neg);
                 if(flg->zero && flg->width > ilength && flg->precision > ilength)
                     ft_putzeros(flg->width - ilength, flenght);
                 else if(flg->precision != -1)
@@ -73,8 +73,8 @@ void ft_resolve_i(int i, t_flag *flg, int *flenght)
                         ft_putblanks(flg->width - flg->precision, flenght);
                     else if(flg->precision > ilength)
                     {
-                        ft_putblanks(flg->width - flg->precision - 1, flenght);
-                        ft_putzeros(flg->precision - ilength + 1, flenght);
+                        ft_putblanks(flg->width - flg->precision , flenght);
+                        ft_putzeros(flg->precision - ilength , flenght);
                     }
                     else if(flg->width > ilength)
                         ft_putblanks(flg->width - ilength, flenght);

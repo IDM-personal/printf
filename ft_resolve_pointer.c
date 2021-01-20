fdb8f5e0b062a9b77b5f2e0db79007a3b9d4641f
root <root@DESKTOP-47KD6QM.localdomain>
2021-01-20 22:25:15 +0100

    ptr

--- ft_resolve_pointer.c
+++ ft_resolve_pointer.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 01:49:32 by idm               #+#    #+#             */
-/*   Updated: 2021/01/20 22:23:49 by root             ###   ########.fr       */
+/*   Updated: 2021/01/20 22:25:12 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,14 +53,14 @@ void ft_resolve_pointer(char *cad, t_flag *flg, int *flenght)
                     if(flg->precision == -1)
                         ft_putblanks(flg->width - ilength, flenght);
                     else if(flg->precision < ilength)
-                        ft_putblanks(flg->width - ilength + 1, flenght);
+                        ft_putblanks(flg->width - ilength, flenght);
                     else
                         ft_putblanks(flg->width - flg->precision - ilength, flenght);
                     ft_putstr("0x", flenght);
                     if(flg->precision > ilength)
                         ft_putzeros(flg->precision - ilength, flenght);
                     else
-                        ft_putstr_n(cad, flg->precision, flenght);
+                        ft_putstr_n(cad, flg->precision + 1, flenght);
                 }
             }
             else

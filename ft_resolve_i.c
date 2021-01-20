611b5119c6f380fe0f3f1b82c7c81bf51920844a
root <root@DESKTOP-47KD6QM.localdomain>
2021-01-20 10:29:45 +0100

    i

--- ft_resolve_i.c
+++ ft_resolve_i.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 19:03:55 by root              #+#    #+#             */
-/*   Updated: 2021/01/20 10:15:28 by root             ###   ########.fr       */
+/*   Updated: 2021/01/20 10:29:23 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,67 @@
 
 void ft_resolve_i_neg(char *cad, t_flag *flg, int *flenght)
 {
-    
+    int ilength;
+
+    ilength = ft_strlen(cad) + 1;
+    if(flg->width != -1 || flg->precision != -1)
+    {
+        if(flg->precision > flg->width)
+        {
+            if(flg->precision >= ilength)
+            {
+                ft_putzeros(flg->precision - ilength, flenght);
+                ft_putstr(cad, flenght);
+            }
+            else
+                ft_putstr(cad, flenght);
+        }
+        else
+        {
+            if(flg->leftjust)
+            {
+                if(flg->precision > ilength)
+                    ft_putzeros(flg->precision - ilength, flenght);
+                if(flg->width != 0 && flg->precision != 0)
+                    ft_putstr(cad, flenght);
+                if(flg->precision != -1)
+                {
+                    if(flg->precision > ilength || flg->precision == 0)
+                        ft_putblanks(flg->width - flg->precision, flenght);
+                    else if(flg->width > ilength)
+                        ft_putblanks(flg->width - ilength, flenght);
+                }
+                else if(ilength < flg->width)
+                    ft_putblanks(flg->width - ilength, flenght);
+            }
+            else
+            {
+                //printf("%i <> %i <> %i <> %i <> %i\n", flg->zero, flg->width, flg->precision, ilength, neg);
+                if(flg->precision != -1)
+                {
+                    if(flg->precision == 0)
+                        ft_putblanks(flg->width - flg->precision, flenght);
+                    else if(flg->precision > ilength)
+                    {
+                        ft_putblanks(flg->width - flg->precision , flenght);
+                        ft_putzeros(flg->precision - ilength , flenght);
+                    }
+                    else if(flg->width > ilength)
+                        ft_putblanks(flg->width - ilength, flenght);
+                }
+                else if(flg->zero == 1 && ilength < flg->width)
+                {
+                    ft_putzeros(flg->width - ilength, flenght);
+                }
+                else if(ilength < flg->width)
+                    ft_putblanks(flg->width - ilength, flenght);
+                if(flg->width != 0 && flg->precision != 0)
+                    ft_putstr(cad, flenght);
+            }
+        }
+    }
+    else
+        ft_putstr(ft_strjoin("-",cad), flenght);
 }
 
 void ft_resolve_i(int i, t_flag *flg, int *flenght)
